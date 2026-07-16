2010aa3bfe39408fb64ed7ad41c28f1c7c8bb945
Hà Hiệp Thanh <hahiepthanhhhtt@gmail.com>
2026-07-16 10:37:20 +0700

    Done clip 24

---
+++ src/test/java/com/hahiepthanh/identity_service/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,96 @@
+package com.hahiepthanh.identity_service.controller;
+
+import com.hahiepthanh.identity_service.dto.request.UserCreationRequest;
+import com.hahiepthanh.identity_service.dto.response.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.LocalDate;
+
+import static java.lang.Math.log;
+
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerIntegrationTest {
+
+    @Container
+    static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry dynamicPropertyRegistry){
+        dynamicPropertyRegistry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        dynamicPropertyRegistry.add("spring.datasource.driverClassName", () -> "com.mysql.cj.jdbc.Driver");
+        dynamicPropertyRegistry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UserCreationRequest userCreationRequest;
+    private UserResponse userResponse;
+    private LocalDate dob;
+
+    @BeforeEach
+    public void initData(){
+        dob = LocalDate.of(2005,06,21);
+        userCreationRequest = UserCreationRequest.builder()
+                .username("thanh")
+                .firstName("Thanh")
+                .lastName("Ha")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("cde25fd38fae")
+                .username("thanh")
+                .firstName("Thanh")
+                .lastName("Ha")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        //GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userCreationRequest);
+
+        //WHEN, THEN
+        var respone = mockMvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username")
+                        .value("thanh"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstName")
+                        .value("Thanh"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastName")
+                        .value("Ha"))
+                ;
+
+        log.info("Result: {}", respone.andReturn().getResponse().getContentAsString());
+    }
+
+}
